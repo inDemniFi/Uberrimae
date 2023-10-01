4f10918ba1a1d7f1e17547217f12a7d0c2d06342
inDemniFi Crypto <118511874+inDemniFi@users.noreply.github.com>
2023-09-30 20:21:57 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default async function Home() {
 
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
-            href="https://github.com/inDemniFi/inDemniFi"
+            href="https://github.com/inDemniFi"
             target="_blank"
             rel="noopener noreferrer"
           >
